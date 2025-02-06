82e498b1cad08641f5c72fb1be1bc4b1a359ad34
sajansuvarnan <sajan.suvarnan@gmail.com>
2025-02-06 14:08:57 +0530

    Initial commit

---
+++ Spring/spring-sec-demo/spring-sec-demo/src/main/java/com/telusko/springsecdemo/HelloController.java
@@ -0,0 +1,14 @@
+package com.telusko.spring_sec_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greet()
+    {
+        return "Hello World";
+    }
+}
---
+++ Spring/spring-sec-demo/spring-sec-demo/src/main/java/com/telusko/springsecdemo/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.telusko.spring_sec_demo.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity )
+    {
+
+    }
+
+
+}
